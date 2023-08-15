34d3608bc240a136603c3067f7b7be577e9e7899
ikoote1 <ikootepreim@gmail.com>
2023-08-15 16:37:27 +0300

    back end setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Hello!",
+  "Hi there!",
+  "Greetings!",
+  "Hey!",
+  "Welcome!"
+]
+
+greetings.each do |message|
+  Greeting.create(message: message)
+end
